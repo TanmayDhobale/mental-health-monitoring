7e2deb60e50b653ac4aa4c65b1ff8ec0a52bbb23
TanmayDhobale <mrspot67@gmail.com>
2024-02-14 11:00:41 +0530

    the image issue fix now user can create accout

--- app/__init__.py
+++ app/__init__.py
@@ -2,35 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_migrate import Migrate  # Import Flask-Migrate
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'e9e475aa6e5be67f240394447f77d62b'
+app.config['SECRET_KEY'] = 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager()
-login_manager.login_view = 'main.login'
+login_manager = LoginManager(app)  # Initialize with app
+login_manager.login_view = 'login'  # Assuming 'login' is the function name or endpoint for your login view
 login_manager.login_message_category = 'info'
 
-
-
+migrate = Migrate(app, db)  # Initialize Flask-Migrate
 
 from app import routes
-
-# app/__init__.py
-from flask_login import LoginManager
-from .models import User
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+from .models import User  # Make sure your models are correctly imported
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'e9e475aa6e5be67f240394447f77d62b'
-    return app
